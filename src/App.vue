eca421a8b2d20261df2ad54d75c046eda006986b
Victor Gois Vieira <victormais08@gmail.com>
2019-06-14 15:15:35 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <button class="btn btn-success btn-lg btn-block" @click="aumentar">Aumentar</button>
+      <button class="btn btn-danger btn-lg btn-block" @click="aumentar2">Aumentar 2</button>
+      <button class="btn btn-warning btn-lg btn-block" @click="diminuir">Diminuir</button>
+      <hr>
+      <button class="btn" :class="[classCSS, {girar: aplicarGirar}]" @click="aplicarC1 = !aplicarC1">CLASS</button>
+      <input type="text" v-model="classCSS">
+      <hr>
+      <div class="alert alert-warning" role="alert">Contador: <strong>{{ contador }} | {{ contador2 }}</strong></div>
+      <div class="alert alert-success" role="alert">Resultado: <strong>{{ resultado }}</strong></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      contador: 0,
+      contador2: 0,
+      aplicarC1: false,
+      classCSS: 'btn-primary btn-lg btn-block',
+      aplicarGirar: false
+    }
+  },
+  computed: {
+    resultado  () {
+      console.log('metodo computed resultado chamado...')
+      return this.contador >= 5 ? 'Maior ou igual a 5' : 'Menor que 5'
+    },
+    estilo1() {
+      return {
+        'btn-primary btn-lg btn-block': this.aplicarC1,
+        'btn-warning btn-lg btn-block': !this.aplicarC1
+      }
+    }
+  },
+  methods: {
+    aumentar() {
+      this.contador++
+    },
+    aumentar2() {
+      this.contador2++
+    },
+    diminuir() {
+      this.contador--
+    }
+  },
+  watch: {
+    contador (novo, antigo) {
+      console.log(novo, antigo)
+      setTimeout(() => {
+        this.contador = 0
+      }, 2000)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .c1 {
+    width: 100%;
+  }
+  .girar {
+    transform: rotate(45deg);
+  }
 </style>
