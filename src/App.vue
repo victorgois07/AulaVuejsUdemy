66f053f6a7415b9404e4a254e8351b59576796b7
Victor Gois Vieira <victormais08@gmail.com>
2019-06-15 17:51:46 -0300

    Condicionais

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,37 @@
 <template>
   <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <button class="btn btn-success btn-lg btn-block" @click="aumentar">Aumentar</button>
-      <button class="btn btn-danger btn-lg btn-block" @click="aumentar2">Aumentar 2</button>
-      <button class="btn btn-warning btn-lg btn-block" @click="diminuir">Diminuir</button>
-      <hr>
-      <button class="btn" :class="[classCSS, {girar: aplicarGirar}]" @click="aplicarC1 = !aplicarC1">CLASS</button>
-      <input type="text" v-model="classCSS">
-      <hr>
-      <div class="alert alert-warning" role="alert">Contador: <strong>{{ contador }} | {{ contador2 }}</strong></div>
-      <div class="alert alert-success" role="alert">Resultado: <strong>{{ resultado }}</strong></div>
+    <div class="list-group">
+      <a href="#" class="list-group-item list-group-item-action" v-for="post in posts">
+        <div class="list-group">
+          <a href="#" class="list-group-item list-group-item-action" v-for="(data, key, index) in post" :key="index">{{data}} - {{key}} - ({{index}})</a>
+        </div>
+      </a>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'app',
   data () {
     return {
-      contador: 0,
-      contador2: 0,
-      aplicarC1: false,
-      classCSS: 'btn-primary btn-lg btn-block',
-      aplicarGirar: false
+      posts: [],
+      errors: []
     }
   },
-  computed: {
-    resultado  () {
-      console.log('metodo computed resultado chamado...')
-      return this.contador >= 5 ? 'Maior ou igual a 5' : 'Menor que 5'
-    },
-    estilo1() {
-      return {
-        'btn-primary btn-lg btn-block': this.aplicarC1,
-        'btn-warning btn-lg btn-block': !this.aplicarC1
-      }
-    }
-  },
-  methods: {
-    aumentar() {
-      this.contador++
-    },
-    aumentar2() {
-      this.contador2++
-    },
-    diminuir() {
-      this.contador--
-    }
-  },
-  watch: {
-    contador (novo, antigo) {
-      console.log(novo, antigo)
-      setTimeout(() => {
-        this.contador = 0
-      }, 2000)
-    }
+  created() {
+    axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados`)
+      .then(response => {
+        this.posts = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
   }
 }
 </script>
 
 <style>
-  .c1 {
-    width: 100%;
-  }
-  .girar {
-    transform: rotate(45deg);
-  }
 </style>
